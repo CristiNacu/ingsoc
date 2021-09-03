9fce7908ec815c5f4f0568f7f377c19081354749
Cristian Nacu <nacu_cristian@yahoo.com>
2021-09-03 21:26:21 +0300

    Initial work on driver initialization + communication

--- IntelPTDriver/IntelPTDriver/Driver.c
+++ IntelPTDriver/IntelPTDriver/Driver.c
@@ -1,8 +1,31 @@
 #include <ntddk.h>
 #include <wdf.h>
+#include <wdfobject.h>
+#include <intrin.h>
+#include "Public.h"
 
 DRIVER_INITIALIZE DriverEntry;
 EVT_WDF_DRIVER_DEVICE_ADD KmdfHelloWorldEvtDeviceAdd;
+
+VOID
+KmdfHelloWorldDriverUnload(
+    IN WDFDRIVER Driver
+)
+{
+    UNREFERENCED_PARAMETER(Driver);
+    DbgBreakPoint();
+}
+
+VOID
+SerialEvtDriverContextCleanup(
+    _In_
+    WDFOBJECT Object
+)
+{
+    UNREFERENCED_PARAMETER(Object);
+    DbgBreakPoint();
+}
+
 
 NTSTATUS
 DriverEntry(
@@ -10,14 +33,19 @@ DriverEntry(
     _In_ PUNICODE_STRING    RegistryPath
 )
 {
+    DbgBreakPoint();
+
     // NTSTATUS variable to record success or failure
-    NTSTATUS status = STATUS_SUCCESS;
+    NTSTATUS status;
+    WDFDRIVER wdfdriver;
 
     // Allocate the driver configuration object
     WDF_DRIVER_CONFIG config;
+    WDF_OBJECT_ATTRIBUTES  attributes;
+
 
     // Print "Hello World" for DriverEntry
-    KdPrintEx((DPFLTR_IHVDRIVER_ID, DPFLTR_INFO_LEVEL, "KmdfHelloWorld: DriverEntry\n"));
+    KdPrintEx((DPFLTR_IHVDRIVER_ID, DPFLTR_ERROR_LEVEL, "KmdfHelloWorld: DriverEntry\n"));
 
     // Initialize the driver configuration object to register the
     // entry point for the EvtDeviceAdd callback, KmdfHelloWorldEvtDeviceAdd
@@ -25,15 +53,116 @@ DriverEntry(
         KmdfHelloWorldEvtDeviceAdd
     );
 
+    config.EvtDriverUnload = KmdfHelloWorldDriverUnload;
+
+
+    WDF_OBJECT_ATTRIBUTES_INIT(
+        &attributes
+    );
+
+    attributes.EvtCleanupCallback = SerialEvtDriverContextCleanup;
+
+
     // Finally, create the driver object
     status = WdfDriverCreate(DriverObject,
         RegistryPath,
-        WDF_NO_OBJECT_ATTRIBUTES,
+        &attributes,
         &config,
-        WDF_NO_HANDLE
+        &wdfdriver
     );
+    if (!NT_SUCCESS(status))
+    {
+        KdPrintEx((DPFLTR_IHVDRIVER_ID, DPFLTR_ERROR_LEVEL, "WdfDriverCreate error\n"));
+        return status;
+    }
+
+    //DbgBreakPoint();
+    //int cpuidIntelpt[4] = { 0 };
+    //__cpuidex(cpuidIntelpt, 0x7, 0x0);
+    //KdPrintEx((DPFLTR_IHVDRIVER_ID, DPFLTR_ERROR_LEVEL, "CPUID(EAX:0x7 ECX:0x0).EBX = %X\n", cpuidIntelpt[1]));
+    //if (cpuidIntelpt[1] & (1 << 25))
+    //{
+    //    KdPrintEx((DPFLTR_IHVDRIVER_ID, DPFLTR_ERROR_LEVEL, "IntelPT Capability Available\n"));
+    //}
+    //else
+    //{
+    //    KdPrintEx((DPFLTR_IHVDRIVER_ID, DPFLTR_ERROR_LEVEL, "IntelPT Capability NOT Available\n"));
+    //}
+    //DbgBreakPoint();
+
     return status;
 }
+
+//////////////////////////////////////////////////////////////////////////// IO QUEUE ////////////////////////////////////////////////////////////////////////////////////
+
+typedef struct _COMM_QUEUE_DEVICE_CONTEXT {
+    PVOID           Data;
+    WDFQUEUE         NotificationQueue;
+    volatile LONG    Sequence;
+} COMM_QUEUE_DEVICE_CONTEXT, * PCOMM_QUEUE_DEVICE_CONTEXT;
+
+VOID
+EchoEvtIoRead(
+    IN WDFQUEUE   Queue,
+    IN WDFREQUEST Request,
+    IN size_t      Length
+)
+{
+    UNREFERENCED_PARAMETER(Queue);
+    UNREFERENCED_PARAMETER(Request);
+    UNREFERENCED_PARAMETER(Length);
+    DbgBreakPoint();
+}
+
+VOID
+EchoEvtIoWrite(
+    IN WDFQUEUE   Queue,
+    IN WDFREQUEST Request,
+    IN size_t     Length
+)
+{
+    UNREFERENCED_PARAMETER(Queue);
+    UNREFERENCED_PARAMETER(Request);
+    UNREFERENCED_PARAMETER(Length);
+    DbgBreakPoint();
+}
+
+VOID
+EchoEvtIoQueueContextDestroy(
+    WDFOBJECT Object
+)
+{
+    UNREFERENCED_PARAMETER(Object);
+    DbgBreakPoint();
+}
+
+NTSTATUS
+EchoQueueInitialize(
+    WDFDEVICE Device
+)
+{
+    WDFQUEUE queue;
+    NTSTATUS status;
+    COMM_QUEUE_DEVICE_CONTEXT queueContext;
+    WDF_IO_QUEUE_CONFIG queueConfig;
+    WDF_OBJECT_ATTRIBUTES attributes;
+
+    PAGED_CODE();
+
+    WDF_IO_QUEUE_CONFIG_INIT_DEFAULT_QUEUE(
+        &queueConfig,
+        WdfIoQueueDispatchSequential
+    );
+
+    queueConfig.EvtIoRead = EchoEvtIoRead;
+    queueConfig.EvtIoWrite = EchoEvtIoWrite;
+
+    WDF_OBJECT_ATTRIBUTES_INIT_CONTEXT_TYPE(&attributes, COMM_QUEUE_DEVICE_CONTEXT);
+
+    return status;
+}
+
+//////////////////////////////////////////////////////////////////////////// END IO QUEUE ////////////////////////////////////////////////////////////////////////////////////
 
 NTSTATUS
 KmdfHelloWorldEvtDeviceAdd(
@@ -41,23 +170,12 @@ KmdfHelloWorldEvtDeviceAdd(
     _Inout_ PWDFDEVICE_INIT DeviceInit
 )
 {
-    // We're not using the driver object,
-    // so we need to mark it as unreferenced
+    __debugbreak();
+    PAGED_CODE();
     UNREFERENCED_PARAMETER(Driver);
+    UNREFERENCED_PARAMETER(DeviceInit);
 
-    NTSTATUS status;
+    KdPrintEx((DPFLTR_IHVDRIVER_ID, DPFLTR_ERROR_LEVEL, "KmdfHelloWorld: KmdfHelloWorldEvtDeviceAdd\n"));
 
-    // Allocate the device object
-    WDFDEVICE hDevice;
-
-    // Print "Hello World"
-    KdPrintEx((DPFLTR_IHVDRIVER_ID, DPFLTR_INFO_LEVEL, "KmdfHelloWorld: KmdfHelloWorldEvtDeviceAdd\n"));
-
-    // Create the device object
-    status = WdfDeviceCreate(&DeviceInit,
-        WDF_NO_OBJECT_ATTRIBUTES,
-        &hDevice
-    );
-    return status;
+    return STATUS_SUCCESS;
 }
-
